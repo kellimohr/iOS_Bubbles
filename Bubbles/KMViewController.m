4c85bc617776ded2240e9688b7321aaa58c524ff
kellimohr <kellimohr@gmail.com>
2013-11-11 15:44:42 -0800

    Added all bubble objects to the view array.

--- Bubbles/KMViewController.m
+++ Bubbles/KMViewController.m
@@ -62,6 +62,8 @@
     
     [self becomeFirstResponder];
     
+    viewArray = [[NSMutableArray alloc] init];
+    
     _animator = [[UIDynamicAnimator alloc] initWithReferenceView: self.view];
     
     _blueBubble = [[UIView alloc] initWithFrame:CGRectMake(200,300,50,50)];
@@ -69,6 +71,7 @@
     _blueBubble.layer.cornerRadius = 25;
     _blueBubble.backgroundColor = [UIColor blueColor];
     [self.view addSubview:_blueBubble];
+    [viewArray addObject:_blueBubble];
     
     _purpleBubble = [[UIView alloc] initWithFrame:CGRectMake(200,400,50,50)];
     _purpleBubble.alpha = 0.5;
@@ -78,6 +81,7 @@
     _purpleBubble.layer.shadowOpacity = 1.0f;
     _purpleBubble.layer.shadowRadius = 8.0f;
     [self.view addSubview:_purpleBubble];
+    [viewArray addObject:_purpleBubble];
     
     _yellowBubble = [[UIView alloc] initWithFrame:CGRectMake(50,400,50,50)];
     _yellowBubble.alpha = 0.5;
@@ -87,6 +91,7 @@
     _yellowBubble.layer.shadowOpacity = 1.0f;
     _yellowBubble.layer.shadowRadius = 8.0f;
     [self.view addSubview:_yellowBubble];
+    [viewArray addObject:_yellowBubble];
     
     _redBubble = [[UIView alloc] initWithFrame:CGRectMake(75,450,50,50)];
     _redBubble.alpha = 0.5;
@@ -96,12 +101,14 @@
     _redBubble.layer.shadowOpacity = 1.0f;
     _redBubble.layer.shadowRadius = 8.0f;
     [self.view addSubview:_redBubble];
+    [viewArray addObject:_yellowBubble];
     
     _greenBubble = [[UIView alloc] initWithFrame:CGRectMake(175,450,50,50)];
     _greenBubble.alpha = 0.5;
     _greenBubble.layer.cornerRadius = 25;
     _greenBubble.backgroundColor = [UIColor greenColor];
     [self.view addSubview:_greenBubble];
+    [viewArray addObject:_greenBubble];
     
     _greenBubbleSmall = [[UIView alloc] initWithFrame:CGRectMake(25,25,10,10)];
     _greenBubbleSmall.alpha = 0.5;
@@ -170,7 +177,10 @@
             anotherView.layer.cornerRadius = 25;
             anotherView.backgroundColor = [UIColor blackColor];
             
-            [viewArray insertObject:bubble atIndex:0];
+            [viewArray addObject:bubble];
+            [viewArray addObject:innerView];
+            [viewArray addObject:anotherView];
+            
             
             [self.view addSubview: bubble];
             [gravityBehavior addItem: bubble];
@@ -203,6 +213,7 @@
         _bigBubble.layer.shadowOpacity = 1.0f;
         _bigBubble.layer.shadowRadius = 8.0f;
         [self.view addSubview:_bigBubble];
+        [viewArray addObject:_bigBubble];
         
         
         UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems: @[_blueBubble, _purpleBubble, _yellowBubble, _redBubble, _greenBubble]];
